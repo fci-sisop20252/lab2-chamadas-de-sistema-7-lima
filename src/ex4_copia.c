2be14db180a9cbc902e0a59a9466f6de6a4bd52c
7-lima <rafael.lima110305@gmail.com>
2025-08-25 12:04:19 -0300

    Update ex4_copia.c

--- src/ex4_copia.c
+++ src/ex4_copia.c
@@ -10,6 +10,18 @@
  * 4. Analise: strace -e openat,read,write,close ./ex4_copia
  */
 
+/*
+ * Exercício 4 - Cópia de Arquivo
+ * 
+ * OBJETIVO: Implementar programa de cópia usando syscalls
+ * 
+ * TAREFA: Complete todos os TODOs
+ * 1. Compile: gcc src/ex4_copia.c -o ex4_copia
+ * 2. Execute: ./ex4_copia
+ * 3. Verifique: diff dados/origem.txt dados/destino.txt
+ * 4. Analise: strace -e open,read,write,close ./ex4_copia
+ */
+
 #include <fcntl.h>
 #include <unistd.h>
 #include <stdio.h>
@@ -29,22 +41,16 @@ int main() {
     printf("=== Exercício 4: Cópia de Arquivo ===\n");
     printf("Copiando: dados/origem.txt -> dados/destino.txt\n\n");
     
-    /*
-     * TODO 1: Abrir arquivo de origem para leitura
-     */
-    fd_origem = /* TODO: abrir dados/origem.txt */;
+    // TODO 1: Abrir arquivo de origem para leitura
+    fd_origem = open("dados/origem.txt", O_RDONLY);
     
     if (fd_origem < 0) {
         perror("Erro ao abrir origem");
         return 1;
     }
     
-    /*
-     * TODO 2: Criar arquivo de destino para escrita
-     * Use flags: O_WRONLY | O_CREAT | O_TRUNC
-     * Permissões: 0644
-     */
-    fd_destino = /* TODO: criar dados/destino.txt */;
+    // TODO 2: Criar arquivo de destino para escrita
+    fd_destino = open("dados/destino.txt", O_WRONLY | O_CREAT | O_TRUNC, 0644);
     
     if (fd_destino < 0) {
         perror("Erro ao criar destino");
@@ -52,47 +58,35 @@ int main() {
         return 1;
     }
     
-    /*
-     * TODO 3: Implementar loop de cópia
-     * read() do origem, write() no destino
-     */
-    while (/* TODO: condição do loop */) {
+    // TODO 3: Implementar loop de cópia
+    while ((bytes_lidos = read(fd_origem, buffer, BUFFER_SIZE)) > 0) {
         total_operacoes++;
         
-        /*
-         * TODO 4: Escrever dados no destino
-         */
-        bytes_escritos = /* TODO: write no destino */;
+        // TODO 4: Escrever dados no destino
+        bytes_escritos = write(fd_destino, buffer, bytes_lidos);
         
-        /*
-         * TODO 5: Verificar se escreveu corretamente
-         */
-        if (/* TODO: verificar bytes_escritos */) {
+        // TODO 5: Verificar se escreveu corretamente
+        if (bytes_escritos != bytes_lidos) {
             perror("Erro na escrita");
             break;
         }
         
-        /*
-         * TODO 6: Atualizar contador
-         */
-        /* TODO: total_bytes += ... */;
+        // TODO 6: Atualizar contador
+        total_bytes += bytes_escritos;
         
         if (total_operacoes % 20 == 0) {
             printf("Progresso: %ld bytes...\n", total_bytes);
         }
     }
     
-    /*
-     * TODO 7: Verificar erro de leitura
-     */
-    if (/* TODO: verificar erro */) {
+    // TODO 7: Verificar erro de leitura
+    if (bytes_lidos < 0) {
         perror("Erro na leitura");
     }
     
-    /*
-     * TODO 8: Fechar ambos os arquivos
-     */
-    /* TODO: close() dos dois file descriptors */;
+    // TODO 8: Fechar ambos os arquivos
+    close(fd_origem);
+    close(fd_destino);
     
     clock_t fim = clock();
     double tempo = ((double)(fim - inicio)) / CLOCKS_PER_SEC;
